68bd48fba0044924431a7c7148b3b1b4ad67ea33
Ivan Ferre <ivan.ifi@hotmail.com>
2025-05-15 12:58:35 +0200

    chore: inicialización proyecto base con Spring Boot para la API

---
+++ src/main/java/com/ferrinsa/fairpartner/FairPartnerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ferrinsa.fairpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairPartnerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairPartnerApiApplication.class, args);
+	}
+
+}
